0c404fb77e65602d0353674d24b1118f3155fb86
jonsalkin <50082962+jonsalkin@users.noreply.github.com>
2023-08-14 11:29:37 -0400

    Initial Commit

---
+++ ScrollViewComplexLayouts/ScrollViewComplexLayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewComplexLayoutsApp.swift
+//  ScrollViewComplexLayouts
+//
+//  Created by Jon Salkin on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewComplexLayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
